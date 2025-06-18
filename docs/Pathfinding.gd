eb3d4ae149fd1ea38a92f15015ef8153e6127f0f
Raven J. <david.phelps.purchase@gmail.com>
2025-06-17 19:41:34 -0600

    Update Pathfinding.gd
    Updated terminologies and debug information

--- docs/Pathfinding.gd
+++ docs/Pathfinding.gd
@@ -50,9 +50,10 @@ var locations := []
 
 var bitter_cold := {}
 var brief_reveal := {}
-var gore := {}
 var bonfire := {}
+var gore := {}
 var human_torch := {}
+var human_popsicle := {}
 var mania := {}
 var paralyze := {}
 var snake_attack := {}
@@ -66,30 +67,33 @@ var ghosts := []
 
 var map1
 
-enum Tag {ADULT,AGING,AIR,BLOOD,CHILD,COLD,CORPSE,CREEPY,CREEPYCRAWLY,DARKNESS,DEATH,DIRTY,EARTH,ELECTRICAL,EMBARRASSMENT,FAILURE,FIRE,GHOST,HEIGHTS,HUNTED,HOT,ICE,INSIDE,ISOLATION,LIGHT,NEEDLES,NOISE,OUTSIDE,PLASMA,REFLECTIVE,REJECTION,SNAKES,SUFFERING,TEENAGER,TRAPPED,WATER,WEAPONS}
+enum Fear {AGING, AIR, BLOOD, BURNING, CORPSE, CREEPYCRAWLY, DARKNESS, DEATH, DIRTY, EMBARRASSMENT, FAILURE, FIRE, FREEZING, GHOST, HEIGHTS, HUNTED, ISOLATION, NEEDLES, NOISE, REJECTION, SNAKES, SUFFERING, TRAPPED, WEAPONS}
+enum Tether {AIR, BLOOD, BURNING, COLD, CORPSE, CREEPY, DARK, DEATH, DIRTY, EARTH, ELECTRICAL, FIRE, FROZEN, HOT, ICE, INSIDE, LIGHT, OUTSIDE, PLASMA, REFLECTIVE, WATER, WEAPON}
+enum Age {CHILD, TEENAGER, ADULT, ELDERLY}
+enum Effect {BLIZZARD, BURNING, COLD, FIRE, FREEZING, FROZEN, HOT, RAIN, SNOW, SWELTERING, TEMPEST, WARM, WINDY}
 enum Gender {CIS_MAN, CIS_WOMAN, NON_BINARY, TRANS_MAN, TRANS_WOMAN}
 enum Orientation {ASEXUAL, BISEXUAL, GAY, LESBIAN, PANSEXUAL, STRAIGHT}
-enum PhysicalStatus {BURNING, COLD, COMATOSE, ELECTROCUTED, FAINTED, FROZEN, HOT, NORMAL, POSSESSED, SLEEPING, SLEEPWALKING, warm}
-enum EmotionalStatus {ANGRY, CONFUSED, CURIOUS, DISGUSTED, HAPPY, INDIFFERENT, SAD, SCARED, TERRIFIED}
+enum PhysicalStatus {BURNING, COLD, COMATOSE, ELECTROCUTED, FAINTED, FROZEN, HOT, NORMAL, POSSESSED, SLEEPING, SLEEPWALKING, WARM}
+enum EmotionalStatus {ANGRY, CONFUSED, CURIOUS, DISGUSTED, HAPPY, INDIFFERENT, NORMAL, SAD, SCARED, TERRIFIED}
 enum TargetType {LINE_OF_SIGHT, MAP, RADIAL, ROOM, SINGLE}
 enum ObjectType {CHARACTER, GHOST, INTERACTIVE_OBJECT, LOCATION, MAP, POWER}
 
-func create_interactive_object(_name:String, _tags:Array):
+func create_interactive_object(_name:String, _tethers:Array):
 	var interactive_object := {
 		object_type = ObjectType.INTERACTIVE_OBJECT,
 		name = _name,
-		tags = _tags
+		tethers = _tethers
 	}
 	interactive_objects.push_back(interactive_object)
 	return interactive_object
 
-func create_location(_name:String, _tags:Array):
+func create_location(_name:String, _tethers:Array):
 	var location := {
 		object_type = ObjectType.LOCATION,
 		name = _name,
 		interactive_objects = [],
 		characters = [],
-		tags = _tags
+		tethers = _tethers
 	}
 	locations.push_back(location)
 	return location
@@ -103,11 +107,11 @@ func create_location(_name:String, _tags:Array):
 	#match location to coordinate values within a certain area
 #	pass
 
-func create_character(_name:String, _tags:Array, _starting_location:Dictionary, _gender:int, _orientation:int, _physical_status:Array, _emotional_status:Array, _fears:Array):
+func create_character(_name:String, _age:int, _starting_location:Dictionary, _gender:int, _orientation:int, _physical_status:Array, _emotional_status:Array, _fears:Array):
 	var character := {
 		object_type = ObjectType.CHARACTER,
 		name = _name,
-		tags = _tags,
+		age = _age,
 		starting_location = _starting_location,
 		gender = _gender,
 		orientation = _orientation,
@@ -119,7 +123,7 @@ func create_character(_name:String, _tags:Array, _starting_location:Dictionary, 
 	add_character_to_starting_location(character)
 	return character
 	
-func create_power(_name:String, _description:String, _cost:int, _duration:int, _base_stats:Array, _target_type:int, _effects:Array):
+func create_power(_name:String, _description:String, _cost:int, _duration:int, _base_stats:Array, _target_type:int, _fears:Array, _effects:Array):
 	var power := {
 		name = _name,
 		description = _description,
@@ -127,6 +131,7 @@ func create_power(_name:String, _description:String, _cost:int, _duration:int, _
 		duration = _duration,
 		base_stats = _base_stats,
 		target_type = _target_type,
+		fears = _fears,
 		effects = _effects
 	}
 	powers.push_back(power)
@@ -146,7 +151,7 @@ func create_map(_name:String, _description:String, _allowed_haunters:Array, _bas
 	var map := {
 		name = _name,
 		description = _description,
-		allowed_haunters = _alowed_haunters,
+		allowed_haunters = _allowed_haunters,
 		base_plasm = _base_plasm,
 		time_goal = _time_goal,
 		personal_best_time = _personal_best_time,
@@ -168,51 +173,52 @@ func add_character_to_starting_location(character:Dictionary):
 	set_location.characters.push_back(character)
 
 func create_all_interactive_objects():
-	toilet = create_interactive_object("Toilet", [Tag.WATER])
-	sink = create_interactive_object("Sink", [Tag.WATER])
-	mirror = create_interactive_object("Mirror", [Tag.REFLECTIVE])
-	bathtub = create_interactive_object("Bathtub", [Tag.WATER])
-	fridge = create_interactive_object("Fridge", [Tag.COLD, Tag.ELECTRICAL])
-	television = create_interactive_object("Television", [Tag.ELECTRICAL, Tag.REFLECTIVE])
-	tree = create_interactive_object("Tree", [Tag.EARTH])
-	logs = create_interactive_object("Logs", [Tag.EARTH])
-	rock = create_interactive_object("Rock", [Tag.EARTH])
-	fireplace = create_interactive_object("Fireplace", [Tag.FIRE, Tag.HOT])
-	pipes = create_interactive_object("Pipes", [Tag.BLOOD, Tag.HOT, Tag.WATER])
-	boiler = create_interactive_object("Boiler", [Tag.ELECTRICAL, Tag.HOT, Tag.WATER])
-	necronomicon = create_interactive_object("Necronomicon", [Tag.BLOOD])
+	toilet = create_interactive_object("Toilet", [Tether.WATER])
+	sink = create_interactive_object("Sink", [Tether.WATER])
+	mirror = create_interactive_object("Mirror", [Tether.REFLECTIVE])
+	bathtub = create_interactive_object("Bathtub", [Tether.WATER])
+	fridge = create_interactive_object("Fridge", [Tether.COLD, Tether.ELECTRICAL])
+	television = create_interactive_object("Television", [Tether.ELECTRICAL, Tether.REFLECTIVE])
+	tree = create_interactive_object("Tree", [Tether.EARTH])
+	logs = create_interactive_object("Logs", [Tether.EARTH])
+	rock = create_interactive_object("Rock", [Tether.EARTH])
+	fireplace = create_interactive_object("Fireplace", [Tether.FIRE, Tether.HOT])
+	pipes = create_interactive_object("Pipes", [Tether.BLOOD, Tether.HOT, Tether.WATER])
+	boiler = create_interactive_object("Boiler", [Tether.ELECTRICAL, Tether.HOT, Tether.WATER])
+	necronomicon = create_interactive_object("Necronomicon", [Tether.BLOOD])
 
 func create_all_characters():
-	ash = create_character("Ash", [Tag.ADULT], living_room, Gender.CIS_MAN, Orientation.STRAIGHT, [PhysicalStatus.NORMAL], [EmotionalStatus.CURIOUS], [Tag.SUFFERING, Tag.REJECTION, Tag.CORPSE])
-	cheryl = create_character("Cheryl", [Tag.ADULT], basement, Gender.NON_BINARY, Orientation.PANSEXUAL, [PhysicalStatus.NORMAL], [EmotionalStatus.CURIOUS, EmotionalStatus.SCARED], [Tag.CREEPYCRAWLY, Tag.CORPSE])
-	linda = create_character("Linda", [Tag.ADULT], first_bathroom, Gender.CIS_WOMAN, Orientation.STRAIGHT, [PhysicalStatus.NORMAL], [EmotionalStatus.SCARED], [Tag.DARKNESS, Tag.DIRTY, Tag.CREEPYCRAWLY, Tag.CORPSE, Tag.DEATH])
-	scott = create_character("Scott", [Tag.ADULT], forest, Gender.TRANS_MAN, Orientation.ASEXUAL, [PhysicalStatus.SLEEPING], [EmotionalStatus.INDIFFERENT], [Tag.DARKNESS, Tag.SUFFERING, Tag.DEATH, Tag.NEEDLES])
-	shelly = create_character("Shelly", [Tag.ADULT], kitchen, Gender.TRANS_WOMAN, Orientation.BISEXUAL, [PhysicalStatus.COLD], [EmotionalStatus.CONFUSED], [Tag.BLOOD, Tag.DEATH])
+	ash = create_character("Ash", Age.ADULT, living_room, Gender.CIS_MAN, Orientation.STRAIGHT, [PhysicalStatus.NORMAL], [EmotionalStatus.CURIOUS], [Fear.SUFFERING, Fear.REJECTION, Fear.CORPSE])
+	cheryl = create_character("Cheryl", Age.ADULT, basement, Gender.NON_BINARY, Orientation.PANSEXUAL, [PhysicalStatus.NORMAL], [EmotionalStatus.CURIOUS, EmotionalStatus.SCARED], [Fear.CREEPYCRAWLY, Fear.CORPSE])
+	linda = create_character("Linda", Age.ADULT, first_bathroom, Gender.CIS_WOMAN, Orientation.STRAIGHT, [PhysicalStatus.WARM], [EmotionalStatus.NORMAL], [Fear.DARKNESS, Fear.DIRTY, Fear.CREEPYCRAWLY, Fear.CORPSE, Fear.DEATH])
+	scott = create_character("Scott", Age.ADULT, bedroom, Gender.TRANS_MAN, Orientation.ASEXUAL, [PhysicalStatus.SLEEPING], [EmotionalStatus.INDIFFERENT], [Fear.DARKNESS, Fear.SUFFERING, Fear.DEATH, Fear.NEEDLES])
+	shelly = create_character("Shelly", Age.ADULT, kitchen, Gender.TRANS_WOMAN, Orientation.BISEXUAL, [PhysicalStatus.COLD], [EmotionalStatus.CONFUSED], [Fear.BLOOD, Fear.DEATH])
 
 func create_all_locations():
-	first_bathroom = create_location("First Bathroom",[Tag.INSIDE])
-	second_bathroom = create_location("Second Bathroom",[Tag.INSIDE])
-	living_room = create_location("Living Room",[Tag.INSIDE])
-	bedroom = create_location("Bedroom",[Tag.INSIDE])
-	basement = create_location("Basement",[Tag.INSIDE, Tag.CREEPY, Tag.DARKNESS])
-	kitchen = create_location("Kitchen",[Tag.INSIDE])
-	forest = create_location("Forest",[Tag.OUTSIDE, Tag.COLD])
+	first_bathroom = create_location("First Bathroom",[Tether.INSIDE])
+	second_bathroom = create_location("Second Bathroom",[Tether.INSIDE])
+	living_room = create_location("Living Room",[Tether.INSIDE])
+	bedroom = create_location("Bedroom",[Tether.INSIDE])
+	basement = create_location("Basement",[Tether.INSIDE, Tether.CREEPY, Tether.DARK])
+	kitchen = create_location("Kitchen",[Tether.INSIDE])
+	forest = create_location("Forest",[Tether.OUTSIDE, Tether.COLD])
 
 func create_all_powers():
-	bitter_cold = create_power("Bitter Cold", "Makes the area and surrounding objects cold.", 15, -1, [0, 0, 1], TargetType.MAP, [Tag.COLD])
-	brief_reveal = create_power("Brief Reveal", "The ghost briefly becomes visible to their target.", 30, 5, [15, 0, 10], TargetType.LINE_OF_SIGHT, [Tag.GHOST])
-	gore = create_power("Gore", "Turns water into blood.", 25, 15, [15, 0, 10], TargetType.RADIAL, [Tag.BLOOD])
-	bonfire = create_power("Bonfire", "Makes the area and surrounding objects hot.", 25, 15, [10, 5, 5], TargetType.RADIAL, [Tag.FIRE, Tag.HOT])
-	human_torch  = create_power("Human Torch", "Target human is set on fire.", 40, 15, [40, 0, 20], TargetType.SINGLE, [Tag.FIRE, Tag.HOT])
-	mania = create_power("Mania", "Increases the target's Madness.", 40, 1, [0, 15, 10], TargetType.SINGLE, [])
-	paralyze = create_power("Paralyze", "Target human is unable to move.", 50, 15, [25, 5, 15], TargetType.SINGLE, [Tag.TRAPPED])
-	snake_attack = create_power("Snake Attack", "A snake briefly appears in front of the target before slithering away.", 30, 10, [15, 5, 0], TargetType.LINE_OF_SIGHT, [Tag.SNAKES])
+	bitter_cold = create_power("Bitter Cold", "Makes the area and surrounding objects cold.", 15, -1, [0, 0, 1], TargetType.MAP, [], [Effect.COLD])
+	bonfire = create_power("Bonfire", "Makes the area and surrounding objects hot.", 25, 15, [10, 5, 5], TargetType.RADIAL, [Fear.FIRE], [Effect.HOT])
+	brief_reveal = create_power("Brief Reveal", "The ghost briefly becomes visible to their target.", 30, 5, [15, 0, 10], TargetType.LINE_OF_SIGHT, [Fear.GHOST], [])
+	gore = create_power("Gore", "Turns water into blood.", 25, 15, [15, 0, 10], TargetType.RADIAL, [Fear.BLOOD], [])
+	human_torch  = create_power("Human Torch", "Target human is set on fire.", 40, 15, [40, 0, 20], TargetType.SINGLE, [Fear.FIRE, Fear.BURNING], [Effect.BURNING, Effect.FIRE, Effect.HOT, Effect.SWELTERING])
+	human_popsicle  = create_power("Human Popsicle", "Target human is frozen in a block of ice.", 40, 15, [40, 0, 20], TargetType.SINGLE, [Fear.FREEZING], [Effect.COLD, Effect.FREEZING, Effect.FROZEN])
+	mania = create_power("Mania", "Increases the target's Madness.", 40, 1, [0, 15, 10], TargetType.SINGLE, [], [])
+	paralyze = create_power("Paralyze", "Target human is unable to move.", 50, 15, [25, 5, 15], TargetType.SINGLE, [Fear.TRAPPED], [])
+	snake_attack = create_power("Snake Attack", "A snake briefly appears in front of the target before slithering away.", 30, 10, [15, 5, 0], TargetType.LINE_OF_SIGHT, [Fear.SNAKES], [])
 
 func create_all_ghosts():
-	frosty = create_ghost("Frosty", 10, [Tag.COLD, Tag.WATER])
-	bernie = create_ghost("Bernie", 10, [Tag.ELECTRICAL, Tag.FIRE, Tag.HOT])
-	medusa = create_ghost("Medusa", 10, [Tag.EARTH, Tag.REFLECTIVE])
-	bloody_mary = create_ghost("Bloody Mary", 10, [Tag.BLOOD, Tag.REFLECTIVE])
+	frosty = create_ghost("Frosty", 10, [Tether.COLD, Tether.WATER])
+	bernie = create_ghost("Bernie", 10, [Tether.ELECTRICAL, Tether.FIRE, Tether.HOT])
+	medusa = create_ghost("Medusa", 10, [Tether.EARTH, Tether.REFLECTIVE])
+	bloody_mary = create_ghost("Bloody Mary", 10, [Tether.BLOOD, Tether.REFLECTIVE])
 	
 func add_all_objects_to_locations():
 	add_object_to_location(bathtub, first_bathroom)
@@ -249,6 +255,7 @@ func add_all_powers_to_ghosts():
 	add_power_to_ghost(bitter_cold, frosty)
 	add_power_to_ghost(brief_reveal, frosty)
 	add_power_to_ghost(gore, frosty)
+	add_power_to_ghost(human_popsicle, frosty)
 	#Bernie
 	add_power_to_ghost(bonfire, bernie)
 	add_power_to_ghost(human_torch, bernie)
@@ -265,7 +272,7 @@ func _ready():
 #	Map-related
 	create_all_interactive_objects()
 	create_all_locations()
-	map1 = create_map("The Dead Evil Cabin in the Forest", "It's so evil and so dead and so cabiny and also a forest.", [frosty, bernie, medusa, bloody_mary] 100, 1200, -1)
+	map1 = create_map("The Dead Evil Cabin in the Forest", "It's so evil and so dead and so cabiny and also a forest.", [frosty, bernie, medusa, bloody_mary], 100, 1200, -1)
 #	Character-related (HAS TO OCCUR AFTER MAP CREATION)
 	create_all_characters()
 #	add_all_characters_to_starting_locations()
@@ -277,7 +284,15 @@ func _ready():
 	add_all_locations_to_map1()
 	add_all_powers_to_ghosts()
 	
+	#For debugging if a ghost can tether to an object with a location
 #	for location in map1.locations:
+#		for ghost in ghosts:
+#			ghost.can_tether = false
 #		for interactive_object in location.interactive_objects:
-#			for tag in interactive_object.tags:
-#				print("Location: %s, Object: %s, Tag: %s" % [location.name, interactive_object.name, Tag.keys()[tag]])
+#			for ghost in ghosts:
+#				for tether in interactive_object.tethers:
+#					if(ghost.tethers.has(tether)):
+#						ghost.can_tether = true
+#						print("%s can tether to %s (%s: %s)."% [ghost.name, location.name, interactive_object.name, Tether.keys()[tether]])
+#						break
+#				print("Location: %s, Object: %s, Tether: %s" % [location.name, interactive_object.name, Tether.keys()[tether]])
